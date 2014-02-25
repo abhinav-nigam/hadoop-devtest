09a40b98502f776a344b6f8329ddfd1f5cb73557
stmishra <stmishra@fastmail.fm>
2014-02-25 08:06:02 +0000

    Vagrantified test

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@ Vagrant.configure("2") do |config|
 
   config.vm.synced_folder  "./", "/vagrant", id: "vagrant-root"
   config.vm.provider "virtualbox" do |v|
-     v.customize ["modifyvm", :id, "--memory", 4096]
+     v.customize ["modifyvm", :id, "--memory", 1024]
      v.customize ["modifyvm", :id, "--cpus", 4]
 	 v.customize ["modifyvm", :id ,"--cpuexecutioncap", "50"]
   end
@@ -14,5 +14,26 @@ Vagrant.configure("2") do |config|
   	puppet.manifests_path = "puppet/manifests"
 	puppet.options = ['--verbose']
 	puppet.module_path = "puppet/modules"
- end
+  end
+
+  config.vm.define :master do |master|
+     master.vm.network :private_network, ip: "10.10.10.1"
+  end
+
+  config.vm.define :backup do |backup|
+    backup.vm.network :private_network, ip: "10.10.10.2"
+  end
+ 
+  config.vm.define :hadoop1 do |hadoop1|
+    hadoop1.vm.network :private_network, ip: "10.10.10.3"
+  end
+ 
+  config.vm.define :hadoop2 do |hadoop2|
+    hadoop2.vm.network :private_network, ip: "10.10.10.4"
+  end
+ 
+  config.vm.define :hadoop3 do |hadoop3|
+    hadoop3.vm.network :private_network, ip: "10.10.10.5"
+  end
+
 end
--- puppet/modules/hadoop/manifests/init.pp
+++ puppet/modules/hadoop/manifests/init.pp
@@ -13,4 +13,82 @@ class hadoop {
 			   creates => "${hadoop_home}-1.2.1",
 			   require => Exec["get_hadoop"]
 		  }
+file {
+  "${hadoop_home}-1.2.1/conf/slaves":
+  source => "puppet:///modules/hadoop/slaves",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["install_hadoop"]
+ }
+ 
+file {
+  "${hadoop_home}-1.2.1/conf/masters":
+  source => "puppet:///modules/hadoop/masters",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["install_hadoop"]
+ }
+
+file {
+  "${hadoop_home}-1.2.1/conf/core-site.xml":
+  source => "puppet:///modules/hadoop/core-site.xml",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["install_hadoop"]
+ }
+ 
+file {
+  "${hadoop_home}-1.2.1/conf/mapred-site.xml":
+  source => "puppet:///modules/hadoop/mapred-site.xml",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["install_hadoop"]
+ }
+ 
+ file {
+  "${hadoop_home}-1.2.1/conf/hdfs-site.xml":
+  source => "puppet:///modules/hadoop/hdfs-site.xml",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec["install_hadoop"]
+ }
+file {
+  "/root/.ssh/id_rsa":
+  source => "puppet:///modules/hadoop/id_rsa",
+  mode => 600,
+  owner => root,
+  group => root,
+  require => Exec['apt-get update']
+ }
+ 
+file {
+  "/root/.ssh/id_rsa.pub":
+  source => "puppet:///modules/hadoop/id_rsa.pub",
+  mode => 644,
+  owner => root,
+  group => root,
+  require => Exec['apt-get update']
+ }
+
+ssh_authorized_key { "ssh_key":
+    ensure => "present",
+    key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDN3S7nKi4MhCwrFjuHcxu08IQMug0lb890xsoi8LxRugPJl1d8pi/ZZfcd8em2KNH2R2RYfiUqbYZYe3BnU+NtMwdYi9+Tf2Kh4yick0WnraeiV9BPfgKhU2JcpmjPqUBYBfmsVHH9q54QixUopCmZ+r+z+M7Ct4ZByFoHOPuiVDHUwRjBeHwTnhF60aTONiGtkPsTqyXWoJvo5zdv3EJ/duaHJ/kr/RhIu7HTP3aGV0PwJSSqwlFpWDDKH3QVnuQW23Ds1GYNDil73UNTh66uy9sf+n9V2XahyZogy9IfszGyvC+sutplVyRigCPz5ad/STWWnPOjhzSNP09DBr8p",
+    type   => "ssh-rsa",
+    user   => "root",
+    require => File['/root/.ssh/id_rsa.pub']
+}
+
+file {
+ "${hadoop_home}-1.2.1/conf/hadoop-env.sh":
+ source => "puppet:///modules/hadoop/hadoop-env.sh.tpl",
+ mode => 644,
+ owner => root,
+ group => root,
+ require => Exec["install_hadoop"]
+}
 }
